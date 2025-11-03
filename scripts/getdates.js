ceaca97e88c2c9ae48645eb02eaadf35fd1b98c2
hengage <henrychizobaudeh@gmail.com>
2025-11-03 14:23:01 +0100

    fix: Update last modified date text  in getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,2 @@
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
-document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
